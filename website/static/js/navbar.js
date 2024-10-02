e24869db8b8fca5c58f90ad20751391cc975601b
nmoussaoui <nmoussaoui@orchestra-premaman.com>
2024-10-02 15:19:07 +0200

    Initial commit

---
+++ website/static/js/navbar.js
@@ -0,0 +1,19 @@
+// static/js/navbar.js
+document.addEventListener('DOMContentLoaded', function () {
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var navbarTogglerIcon = document.querySelector('.navbar-toggler-icon');
+
+    navbarToggler.addEventListener('click', function () {
+        if (navbarToggler.classList.contains('collapsed')) {
+            navbarTogglerIcon.style.backgroundImage = "url('/static/icons/cross.svg')";
+        } else {
+            navbarTogglerIcon.style.backgroundImage = "url('/static/icons/burger.svg')";
+        }
+    });
+
+    if (!navbarToggler.classList.contains('collapsed')) {
+        navbarTogglerIcon.style.backgroundImage = "url('/static/icons/cross.svg')";
+    } else {
+        navbarTogglerIcon.style.backgroundImage = "url('/static/icons/burger.svg')";
+    }
+});
