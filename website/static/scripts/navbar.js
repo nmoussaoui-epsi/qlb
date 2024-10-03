3d233742114b0af6553416012968015ee5c57616
nmoussaoui-epsi <nassim.moussaoui@ecoles-epsi.net>
2024-10-03 22:35:19 +0200

    Update background size in about and services sections

--- website/static/scripts/navbar.js
+++ website/static/scripts/navbar.js
@@ -3,16 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
     var navbarTogglerIcon = document.querySelector('.navbar-toggler-icon');
 
     navbarToggler.addEventListener('click', function () {
-        if (navbarToggler.classList.contains('collapsed')) {
-            navbarTogglerIcon.style.backgroundImage = "url('/static/icons/cross.svg')";
-        } else {
-            navbarTogglerIcon.style.backgroundImage = "url('/static/icons/burger.svg')";
-        }
+        navbarTogglerIcon.style.backgroundImage = navbarToggler.classList.contains('collapsed') 
+            ? "url('/static/icons/cross.svg')" 
+            : "url('/static/icons/burger.svg')";
     });
 
-    if (!navbarToggler.classList.contains('collapsed')) {
-        navbarTogglerIcon.style.backgroundImage = "url('/static/icons/cross.svg')";
-    } else {
-        navbarTogglerIcon.style.backgroundImage = "url('/static/icons/burger.svg')";
-    }
+    navbarTogglerIcon.style.backgroundImage = navbarToggler.classList.contains('collapsed') 
+        ? "url('/static/icons/burger.svg')" 
+        : "url('/static/icons/cross.svg')";
+
+    window.addEventListener('scroll', function() {
+        var navbar = document.querySelector('.navbar');
+        navbar.classList.toggle('fixed-top', window.scrollY > 50);
+    });
 });
