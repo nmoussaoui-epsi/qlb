ce24a95de1a38f9b93160b25e0b5f0fca8974ab0
nmoussaoui <nmoussaoui@orchestra-premaman.com>
2024-10-04 10:26:17 +0200

    update legals

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('terms-and-conditions/', views.terms_and_conditions, name='terms_and_conditions'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def landing_page(request):
     return render(request, 'index.html')
+
+def terms_and_conditions(request):
+    return render(request, 'terms_and_conditions.html')
+
+def privacy_policy(request):
+    return render(request, 'privacy_policy.html')
